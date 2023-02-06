8063e3553e67e67647493aea7db7646473c2f93a
hasesho05 <midorinotanuki56@gmail.com>
2023-02-06 23:22:54 +0900

    グラフ追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,11 @@ import CircularProgress from '@mui/material/CircularProgress';
 import { useCallback, useEffect, useState } from 'react'
 import Header from '../components/Header'
 import apiAccess from './api/api';
+import { Bar } from 'react-chartjs-2';
+import { Chart, registerables } from "chart.js"
+
+Chart.register(...registerables)
+
 
 export default function Home() {
   const [isHeaderShown, setIsHeaderShown] = useState(true);
@@ -10,6 +15,9 @@ export default function Home() {
   const [text, setText] = useState<string>("")
   const [tweetList, setTweetList] = useState([])
   const [loading, setLoading] = useState(false)
+  const [wordData, setWordData] = useState([])
+  const [graphData, setGraphData] = useState<any>("")
+  const [options, setOptions] = useState<any>("")
   const headerHeight = 40;
 
   const scrollEvent = useCallback(() => {
@@ -25,17 +33,19 @@ export default function Home() {
   } 
 
   const onSubmit = () => {
+    setWordData([])
     setLoading(true)
     setTweetList([])
     const payload = {
       id: text
     };
 
-    // const funcSuccess = (response: any) => {
-    //   setTweetList(response.data)
-    //   setLoading(false)
-    // }
-    // apiAccess('CHECK', funcSuccess, payload);
+    const funcSuccess = (response: any) => {
+      setTweetList(response.data.tweetList)
+      setWordData(response.data.counter)
+      setLoading(false)
+    }
+    apiAccess('CHECK', funcSuccess, payload);
   }
 
   useEffect(() => {
@@ -46,19 +56,85 @@ export default function Home() {
   }, [scrollEvent]);
 
   useEffect(() => {
-    console.log(tweetList);
+    let word:Array<string> = []
+    let number:Array<number> = []
+    wordData.forEach((data: any) => {
+      word.push(data[0])
+      number.push(data[1])
+    })
     
-  },[tweetList])
+    setGraphData({
+      // x 軸のラベル
+      labels: [...word],
+      datasets: [
+        {
+          label: '単語の出現回数',
+          // データの値
+          data: [...number],
+          // グラフの背景色
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 205, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgb(235, 186, 245, 0.2)',
+            'rgb(245, 201, 186, 0.2)',
+            'rgb(245, 235, 186, 0.2)',
+            'rgb(196, 245, 186, 0.2)',
+          ],
+          // グラフの枠線の色
+          borderColor: [
+            'rgb(255, 99, 132)',
+            'rgb(255, 159, 64)',
+            'rgb(255, 205, 86)',
+            'rgb(75, 192, 192)',
+            'rgb(54, 162, 235)',
+            'rgb(153, 102, 255)',
+            'rgb(235, 186, 245)',
+            'rgb(245, 201, 186)',
+            'rgb(245, 235, 186)',
+            'rgb(196, 245, 186)',
+          ],
+          // グラフの枠線の太さ
+          borderWidth: 1,
+        },
+      ],
+      options: {
+        scales: {
+          "yaxes_1" : {
+            beginAtZero: true
+          }
+        }
+    }
+    })
+
+  },[wordData])
+
+  useEffect(() => {
+
+    setOptions({
+      responsive: true,
+      plugins: {
+        title: {
+          display: true,
+          text: text,
+        },
+      },
+    })
+  },[text])
+
+
 
   return (
-
     <Container>
-      <Box sx={{mx:"auto", maxWidth:"700px", backgroundColor:"lightgray", height:"100%"}}>
+      <Box sx={{mx:"auto", maxWidth:"700px", backgroundColor:"#f2f2f2", height:"100%"}}>
         {isHeaderShown && <Header />}
         <Box p={10}>
           <Stack spacing={3}>
-            <Typography variant='h1' fontSize={30}>ユーザーIDを入力</Typography>
-            <TextField required label="user_id" type="text" onChange={(e)=>handleText(e)}/>
+            <Typography variant='h1' fontSize={30}>英単語を入力</Typography>
+            <TextField required label="word" type="text" onChange={(e)=>handleText(e)}/>
             <Button 
               sx={[{backgroundColor:"black"},()=>({'&:hover': {backgroundColor:"black"}})]}  
               variant="contained" 
@@ -68,15 +144,16 @@ export default function Home() {
               Create
             </Button>
             {loading && 
-            <Box sx={{width:"100%"}}>
-              <CircularProgress sx={{mx:"auto",mt:"50px",color:"gray"}}/>
-            </Box>
+              <Box sx={{width:"100%"}}>
+                <CircularProgress sx={{ml:"250px",color:"gray"}}/>
+              </Box>
             }
           </Stack>
 
           <Box>
-            {tweetList?.map((tweet: any) => (
-              <Box sx={{backgroundColor:"white", borderRadius:2, p:2, mt:2}}>
+            {graphData && options && <Bar data={graphData} options={options}/>}
+            {tweetList?.map((tweet: string, index:number) => (
+              <Box key={index} sx={{backgroundColor:"white", borderRadius:2, p:2, mt:2}}>
                 <Typography color="black">{tweet}</Typography>
               </Box>
             ))}
